25839e4e5caae933420d0d11bd0953213dcef6ee
Pedro Bonafé <bonafepedro@gmail.com>
2021-02-01 23:06:33 -0300

    primer commit

---
+++ generador_contrasena.py
@@ -0,0 +1,27 @@
+import random
+
+
+def generar_contrasena():
+    mayusculas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K']
+    minusculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']
+    simbolos = ['!', '#', '$', '%', '&', '/', '(', ')', '=' , '+',]
+    numeros = ['1', '2', '3', '4', '5','6', '7', '8', '9', '0' ]
+
+    caracteres = mayusculas + minusculas + simbolos + numeros
+
+    contrasena = []
+
+    for i in range(15):
+        caracter_random = random.choice(caracteres)
+        contrasena.append(caracter_random)
+
+    contrasena = ''.join(contrasena)
+    return contrasena
+
+
+def main ():
+    contrasena= generar_contrasena()
+    print (' Tu nueva contraseña es: ' + contrasena)
+
+
+main()
